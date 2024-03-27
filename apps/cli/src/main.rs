e8ba555dbdccd0b431f1fe92960acb937fd5f655
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2024-03-27 09:41:36 +0900

    making development

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -11,12 +11,38 @@ struct Args {
     /// Number of times to greet
     #[arg(short, long, default_value_t = 1)]
     count: u8,
+
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// docker-compose
+    Development,
+
+    /// CDK
+    Cdk,
+}
+
+
+fn execute_development() {
+    std::fs::create_dir_all("infra/development").unwrap();
 }
 
 fn main() {
+    let path = std::env::current_dir().unwrap();
+    println!("starting dir: {}", path.display());
+    execute_development();
+
     let args = Args::parse();
 
     for _ in 0..args.count {
         println!("Hello {}!", args.name)
+    }
+
+    match args.command {
+        Command::Development => println!("development"),
+        Command::Cdk => println!("cdk"),
     }
 }
